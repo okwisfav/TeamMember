985256b03c83e4e816b3671ef1a14de3c09b8592
ofego favour <ofegofavour@gmail.com>
2022-09-12 15:40:55 +0100

    team

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -89,12 +89,26 @@ const Employees = () => {
       }]);
   return (
     <main className="container"> 
-       <div className='row'>
+     <div className='row justify-content-center mt-3 mb-3'>
             <div className="col-8">
+               <select>
+                 <option>Team A</option>
+                 <option>Team B</option>
+                 <option>Team C</option>
+                 <option>Team D</option>
+               </select>
+            </div>
+        </div>
+       <div className='row justify-content-center mt-3 mb-3'>
+            <div className="col-8">
+              <div className='card-collection'>
                 {
                   employees.map((employee) => (
-                    <div id={employee.id} className="card">
-                         <img src={femaleProfile} className="cad-img-top" />
+                    <div id={employee.id} className="card m-2" style={{cursor:"pointer"}}>
+
+                         {(employee.gender === 'male')?<img src={maleProfile} className="cad-img-top" />
+                                                      :<img src={femaleProfile} className="cad-img-top" />  }
+
                          <div className='card-body'>
                              <h5 className='card-title'>Full Name: {employee.fullName}</h5>
                              <p className='card-text'><b>Designation:</b>{employee.designation}</p>
@@ -102,6 +116,7 @@ const Employees = () => {
                     </div> 
                   ))
                 }
+                </div>
             </div>
        </div>
     </main>
